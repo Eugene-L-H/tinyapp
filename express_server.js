0f8475fd4e012a3d75b7972be301388b6fde921d
Eugene Helland <eugenehelland@mail.com>
2022-03-22 11:12:31 -0600

    tiny links now link to their full link counterparts when clicked.

--- express_server.js
+++ express_server.js
@@ -28,6 +28,11 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
+
 // redirect to show all URLs page
 app.get('/urls', (req, res) => {
   const templateVars = { urls: urlDatabase };
